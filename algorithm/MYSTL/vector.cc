1fe9b023dfa41c5db86e2589a59c536b44d8baa3
96189 <39409849+96189@users.noreply.github.com>
2018-07-25 20:15:09 +0800

    MYSTL vector部分实现

--- algorithm/MYSTL/vector.cc
+++ algorithm/MYSTL/vector.cc
@@ -1,4 +1,7 @@
 #include "vector.h"
+#include <string.h>
+#include <assert.h>
+#include <stdlib.h>
 
 using namespace MYSTL;
 
@@ -6,17 +9,40 @@ using namespace MYSTL;
 template <typename T>
 void copyFrom(const T *A, Rank lo, Rank hi)
 {
-
+    // 
+    Rank count = hi - lo;
+    _capacity = count << 1;
+    _elem = new T[_capacity];
+    assert(_elem);
+    for (_size = 0; _size < count; ++_size)
+    {
+        _elem[_size] = A[lo + _size];
+    }  
 }
 // 空间不足时扩容
+template <typename T>
 void expand()
 {
-
+    if (_size == _capacity)
+    {
+        T* newAddr = new T[_capacity=<<1];
+        assert(newAddr);
+        memcpy(newAddr, _elem, _size * sizeof(T));
+        delete[] _elem;
+        _elem = newAddr;
+    }
 }
 // 装填因子过小时压缩
 void shrink()
 {
-
+    if (_size * 2 < _capacity)
+    {
+        T* newAddr = new T[_capacity=>>1];
+        assert(newAddr);
+        memcpy(newAddr, _elem, _size * sizeof(T));
+        delete[] _elem;
+        _elem = newAddr;        
+    }
 }
 // 扫描交换
 bool bubble(Rank lo, Rank hi)
@@ -31,7 +57,15 @@ bool bubbleSort(Rank lo, Rank hi)
 // 选取最大元素
 Rank max(Rank lo, Rank hi)
 {
-    return 0;
+    Rank maxIdx = hi - 1;
+    for (int idx = maxIdx - 1; idx >= lo; --idx)
+    {
+        if (_elem[idx] > _elem[maxIdx])
+        {
+            maxIdx = idx;
+        }
+    }
+    return maxIdx;
 }
 // 选择排序
 bool selectionSort(Rank lo, Rank hi)
@@ -64,49 +98,100 @@ void heapSort(Rank lo, Rank hi)
 
 }
 
-// 判断向量是否已排序
+// 判断向量是否已排序(升序)
 int disordered() 
 {
-    return 0;
+    int counter = 0;
+    for (Rank idx = 1; idx < _size; ++idx)
+    {
+        counter += _elem[idx - 1] > _elem[idx] ? 1 : 0;
+    }
+    return counter;
 }
 // 无需向量区间查找
 template <typename T>
 Rank find(const T &e, Rank lo, Rank hi) 
 {
-    return 0;
+    Rank pos = -1;
+    for (int i = lo; i < hi; ++i)
+    {
+        if (_elem[i] == e)
+        {
+            pos = i;
+            break;
+        }
+    }
+    return pos;
 }
 // 有序向量区间查找
 template <typename T>
 Rank search(const T &e, Rank lo, Rank hi)
 {
-    return 0;
+    Rank pos = -1;
+    Rank mi = 0;
+    while (lo <= hi)
+    {
+        // 二分查找 选取中间位置
+        mi = (lo + hi) >> 1;
+        // fibonacii查找 选取黄金分割点
+        if (e < _elem[mi])
+        {
+            hi = mi - 1;
+        }
+        else if (_elem[mi] < e)
+        {
+            lo = mi + 1;
+        }
+        else 
+        {
+            pos = mi;
+            break;
+        }
+    }
+    return pos;
 }
 
 // 可写访问接口
-// template <typename T>
-// T &operator[](Rank r) const
-// {
-//     return 0;
-// }
-// template <typename T>
-// vector<T> &operator=(const vector<T> &V)
-// {
-// }
+template <typename T>
+T &operator[](Rank r) const
+{
+    return static_cast<T*>(_elem + r);
+}
+template <typename T>
+vector<T> &operator=(const vector<T> &V)
+{
+    if (this == &V) return;
+    copyFrom(V._elem, 0, V._size);
+    delete[] V._elem;
+    V._elem = NULL;
+    return *this;
+}
 // 删除某一位置的值
-// template <typename T>
-// T remove(Rank r)
-// {
-
-// }
+template <typename T>
+T remove(Rank r)
+{
+    T val = _elem[r];
+    remove(r, r + 1);
+    return val;
+}
 // 删除一段区间
 int remove(Rank lo, Rank hi)
 {
-    return 0;
+    if (lo == hi) return 0;
+    while (hi < _size)
+    {
+        _elem[lo++] = _elem[hi++];
+    }
+    return (hi - lo);
 }
 // 插入元素
 template <typename T>
 Rank insert(Rank r, const T &e)
 {
+    expand();
+    // memcpy解决内存重叠问题
+    memcpy(_elem + r, _elem + r + 1, (_size - r) * sizeof(T));
+    _elem[r] = e;
     return 0;
 }
 
@@ -119,18 +204,34 @@ void sort(Rank lo, Rank hi)
 // 区间置乱
 void unsort(Rank lo, Rank hi)
 {
-
+    for (Rank i = hi - 1; i >= lo; --i)
+    {
+        Rank r = rand() % i;
+        T tmp = _elem[i];
+        _elem[i] = _elem[r];
+        _elem[r] = tmp;
+    }
 }
 
 // 无序去重
 int deduplicate()
 {
+    
     return 0;
 }
 // 有序去重
 int uniquify()
 {
-    return 0;
+    Rank i = 0;
+    for (j = 1; j < _size; ++j)
+    {
+        if (_elem[i] != _elem[j])
+        {
+            _elem[++i] = _elem[j];
+        }
+    }
+    _size = i;
+    return i;
 }
 
 // 遍历
