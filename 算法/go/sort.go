e5a5fe9b2305ece86a55908cb5d8028f9603d01d
96189 <757806890@qq.com>
2019-10-08 17:25:12 +0800

    go版本基本排序算法及二分查找实现

--- 算法/go/sort.go
+++ 算法/go/sort.go
@@ -1,7 +1,34 @@
 
 package baseSort
 
-func bubbleSort(seq	[]int) {
+// 冒泡排序
+// 时间复杂度O(n*n)
+// 空间复杂度O(1)
+// 稳定性:稳定
+// 最好情况下(序列本身有序)时间复杂度:O(n)
+func BubbleSort(seq []int) {
+	n := len(seq)
+	for i := 0; i < n; i++ {
+		swapCount := 0
+		for j := n-1; j > i; j-- {
+			// 两两交换 一次循环交换多次
+			if seq[j] < seq[j-1] {
+				seq[j], seq[j-1] = seq[j-1], seq[j]
+				swapCount++
+			}
+		}
+		if swapCount == 0 {
+			break
+		}
+	}
+}
+
+// 选择排序
+// 时间复杂度O(n*n)
+// 空间复杂度O(1)
+// 稳定性:不稳定
+// 没有最好的情况 算法复杂度始终为O(n*n)
+func SelectSort(seq	[]int) {
 	n := len(seq)
 	for i := 0; i < n; i++ {
 		minIdx := i
@@ -10,8 +37,28 @@ func bubbleSort(seq	[]int) {
 				minIdx = j
 			}
 		}
+		// 选择极值交换 一次循环最多只交换一次
 		if minIdx != i {
 			seq[i], seq[minIdx] = seq[minIdx], seq[i]
+		}
+	}
+}
+
+// 插入排序
+// 时间复杂度O(n*n)
+// 空间复杂度O(1)
+// 稳定性:稳定
+// 最好的情况下(序列本身有序)时间复杂度:O(n)
+func InsertSort(seq []int) {
+	n := len(seq)
+	// uIdx无序起始位置 nextIdx有序的下一个位置
+	for uIdx := 1; uIdx < n; uIdx++ {
+		for nextIdx := uIdx; nextIdx > 0; nextIdx-- {
+			if seq[nextIdx-1] > seq[nextIdx] {
+				seq[nextIdx-1], seq[nextIdx] = seq[nextIdx], seq[nextIdx-1]
+			} else {
+				break
+			}
 		}
 	}
 }
--- 算法/go/sort_test.go
+++ 算法/go/sort_test.go
@@ -4,16 +4,47 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func TestSelectSort(t *testing.T) {
+	useAssert := assert.New(t)
+	s := [][]int{
+		{5,8,5,2,9},
+		{5,5,8,2,9},
+		{2,5,5,8,9},	
+		{9,8,5,5,2},	
+	}
+	d := []int{2,5,5,8,9}
+	for _, seq := range s {
+		SelectSort(seq)
+		useAssert.Equal(d, seq, "sortSuccess")
+	}
+}
+
 func TestBubbleSort(t *testing.T) {
 	useAssert := assert.New(t)
-	s := [3][]int{
-		{1,1,2,2,3,3,4,4,5,5},
-		{5,5,4,4,3,3,2,2,1,1},
-		{1,2,3,4,5,1,2,3,4,5},		
+	s := [][]int{
+		{5,8,5,2,9},
+		{5,5,8,2,9},
+		{2,5,5,8,9},	
+		{9,8,5,5,2},	
 	}
-	d := []int{1,1,2,2,3,3,4,4,5,5}
+	d := []int{2,5,5,8,9}
 	for _, seq := range s {
-		bubbleSort(seq[:])
+		BubbleSort(seq)
+		useAssert.Equal(d, seq, "sortSuccess")
+	}
+}
+
+func TestInsertSort(t *testing.T) {
+	useAssert := assert.New(t)
+	s := [][]int{
+		{5,8,5,2,9},
+		{5,5,8,2,9},
+		{2,5,5,8,9},	
+		{9,8,5,5,2},	
+	}
+	d := []int{2,5,5,8,9}
+	for _, seq := range s {
+		InsertSort(seq)
 		useAssert.Equal(d, seq, "sortSuccess")
 	}
 }
